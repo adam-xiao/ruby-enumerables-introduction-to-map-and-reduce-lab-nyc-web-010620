a681f8da9d6f2feff2002bdcd0a9c6a8cd3f615c
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-02 06:50:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map_to_negativize(source_array)
   new_array = []
 
   while x < source_array.length do
-    new_array = source_array[x] * -1
+    new_array[x] = source_array[x] * -1
     x += 1
   end
   new_array
