9503192114bd21acf3aa76ed79560f37fec8f9f4
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-02 06:53:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,3 +12,13 @@ end
 def map_to_no_change(source_array)
   new_array = source_array
 end
+
+def map_to_double(source_array)
+  x = 0
+  new_array = []
+
+  while x < source_array.length do
+    new_array[x] = source_array[x] * 2
+    x += 1
+  end
+  new_array
